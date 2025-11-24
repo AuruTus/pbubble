e42ab1f617964f97708a84fab1f1d9ed9a0bc115
AuruTus <emslhy@hotmail.com>
2025-11-24 21:48:54 +0800

    test: refactor: using test loop for 1f1b
    Signed-off-by: AuruTus <emslhy@hotmail.com>

--- tests/test_1f1b.rs
+++ tests/test_1f1b.rs
@@ -5,10 +5,22 @@ use pbubble::utils::print_arrangements_matrix;
 
 #[test]
 fn test_1f1b() -> Result<()> {
-    let mut scheduler = Scheduler::new(4);
-    let arrangements = scheduler.run::<Strategy1F1B>(8)?;
+    let test_cases = vec![
+        (2, 4),
+        (4, 8),
+        (4, 10),
+        (4, 6),
+        (4, 16),
+    ];
+    for (world_size, num_minibatch) in test_cases {
+        let split_mark = "=".repeat(20);
 
-    print_arrangements_matrix(arrangements.as_slice());
+        println!("{} world_size: {}, num_minibatch: {} {}", split_mark, world_size, num_minibatch, split_mark);
+        let mut scheduler = Scheduler::new(world_size);
+        let arrangements = scheduler.run::<Strategy1F1B>(num_minibatch)?;
+        print_arrangements_matrix(arrangements.as_slice());
+        println!("\n");
+    }
 
     Ok(())
 }
